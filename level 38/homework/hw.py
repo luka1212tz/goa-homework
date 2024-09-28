9b368b357704576e6f94f22ea67eb69b8982026f
luka1212tz2 <15nlluksboy@gmail.com>
2024-09-28 21:12:05 +0400

    third commit

---
+++ level 38/homework/hw.py
@@ -0,0 +1,16 @@
+listn = ["g","o","a","i","s","b","e","s","t","a","c","a","d","e","m","y"]
+
+print("".join(listn))
+
+
+print("-".join(listn))
+
+
+print("_".join(listn))
+
+#codewars
+#https://www.codewars.com/kata/546e2562b03326a88e000020/train/python
+#https://www.codewars.com/kata/56747fd5cb988479af000028/train/python
+#https://www.codewars.com/kata/54ba84be607a92aa900000f1/train/python
+#https://www.codewars.com/kata/5667e8f4e3f572a8f2000039/train/python
+#https://www.codewars.com/kata/554e4a2f232cdd87d9000038/train/python
