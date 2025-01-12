535605a19e22b6e679424bdc07057840dfae2112
luka1212tz2 <15nlluksboy@gmail.com>
2025-01-12 18:33:21 +0400

    twenty-fiveth commit

---
+++ level 71/classwork/script.js
@@ -0,0 +1,17 @@
+
+console.log(30*24*60*60)
+alert(30*24*60*60)
+
+let days = 30   
+let hours = 24
+let minutes = 60
+let seconds = 60
+
+alert(days * hours * minutes * seconds)
+console.log(days * hours * minutes * seconds)
+
+
+let width = 8
+let height = 14
+
+console.log(width * height)
