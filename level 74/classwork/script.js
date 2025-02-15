ff14a5212305c699ab90502bcbc469ff77153d97
luka1212tz2 <15nlluksboy@gmail.com>
2025-02-15 10:33:47 +0400

    Thirty-first commit

--- level 74/classwork/script.js
+++ level 74/classwork/script.js
@@ -13,4 +13,56 @@ switch (choice) {
     default:
         console.log("Unknown");
         break;
-}
+    }
+
+    function switchItUp(number){
+
+        switch (number) {
+            case 0:
+              return "Zero";
+              break;
+            case 1:
+              return "One";
+              break;
+            case 2:
+              return "Two";
+              break;
+            case 3:
+              return "Three";
+              break;
+            case 4:
+              return "Four";
+              break;
+            case 5:
+              return "Five";
+              break;
+            case 6:
+              return "Six";
+              break;
+            case 7:
+              return "Seven";
+              break;
+            case 8:
+              return "Eight";
+              break;
+            case 9:
+              return "Nine";
+              break;
+            default:
+              return "Invalid input";
+          }
+        }
+
+
+        function getCoffeeType(choice) {
+            switch (choice) {
+                case 1:
+                    return "Americano";
+                case 2:
+                    return "Espresso";
+                case 3:
+                    return "Cappuccino";
+                default:
+                    return "Unknown";
+            }
+        }
