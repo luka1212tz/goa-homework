0a832a2ce35fcde259e401fe9cf95db7d787cbb7
luka1212tz2 <15nlluksboy@gmail.com>
2025-02-23 20:43:04 +0400

    Thirty-Second commit

---
+++ level 83/classwork/script.js
@@ -0,0 +1,20 @@
+
+
+let name = prompt("Your name ");  
+
+function hello(name) {
+    console.log("Welcome,", name);
+}
+
+hello();
+
+
+
+let feet = number(prompt("how many feet"))
+
+function feetToInches(feet) {
+    console.log(feet * 12);
+     
+}
+
+feetToInches(feet)
