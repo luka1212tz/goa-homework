f5e0606479a257777a519786c9ee974906ebdbb9
luka1212tz2 <15nlluksboy@gmail.com>
2025-03-01 17:01:01 +0400

    Thirty-Fourth commit

---
+++ level 85/classwork/script.js
@@ -0,0 +1,16 @@
+function mouthSize(animal){
+    if(animal.toLowerCase() == "alligator"){
+      return ("small")
+    }
+    else{
+      return ("wide")
+    }
+  }
+
+
+
+  function past(h, m, s){
+    return (h * 3600000 + m * 60000 + s * 1000) 
+  }
+
+
