4cf1bd0bd10d996ed84c8abd111af57bb6e01a8a
luka1212tz <lukamodeluka12@gmail.com>
2025-03-21 21:01:16 +0400

    sixth commit

---
+++ level 90/homework/script.js
@@ -0,0 +1,20 @@
+
+let Array = [ 25,  "Hello", false,  { name: "Luka" },[1, 2, 3],  99, "JavaScript",    true,  { age: 30 },     ["apple", "banana"] ];
+
+
+console.log("First 5 values:");
+console.log(mixedArray[0]);
+console.log(mixedArray[1]);
+console.log(mixedArray[2]);
+console.log(mixedArray[3]);
+console.log(mixedArray[4]);
+
+
+mixedArray[5] = 50;                     
+mixedArray[6] = "Updated String";      
+mixedArray[7] = false;                  
+mixedArray[8] = { city: "Tbilisi" };    
+mixedArray[9] = ["grape", "orange"];    
+
+
+console.log("Updated Array:", Array);
