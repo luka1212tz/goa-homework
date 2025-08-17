74e57ad608d2ef58e0ece62102e4ca6391b6535c
luka1212tz <lukamodeluka12@gmail.com>
2025-08-17 21:02:44 +0400

    forty-second commit

--- level 131/home/script.js
+++ level 131/home/script.js
@@ -3,18 +3,15 @@ console.log(2);
 console.log(3);
 
 
-function func() {
-  let sum = 0;
-  for (let i = 0; i < 11; i++) {
-    sum += i;
-  }
-  return sum;
-}
+
+setTimeout(() => console.log(2),3000);
+
 
 console.log(1);
 console.log(func())
 console.log(3);
 
 
-let name = prompt("saxeli:"); 
-console.log("hi,", name);
+
+//სინქრონული: კოდი მუშაობს რიგრიგობით ყოველი ოპერაცია ელოდება წინას
+//არასინქრონული: კოდი არ ელოდება აგრძელებს სხვას და შედეგი მოგვიანებით ბრუნდება
