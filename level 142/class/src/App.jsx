87f547697f328c61a15206e534c01da168cca5b4
luka1212tz <lukamodeluka12@gmail.com>
2025-09-21 16:27:38 +0400

    Fifty-First commit

---
+++ level 142/class/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './index.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className="text-red-500">
+    Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
+
