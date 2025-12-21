eefcce189a407ddd0a2a607f34b5c779493c9986
luka1212tz <lukamodeluka12@gmail.com>
2025-12-21 20:40:01 +0400

    Fifty-Eighth commit

---
+++ level 167/class/src/App.jsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+
+
+  return (
+    <>
+      <div className="text-2xl text-red-300 ">
+      hello my name is
+      </div>
+      <div className="text-amber-800">luka</div>
+    </>
+  )
+}
+
+export default App
