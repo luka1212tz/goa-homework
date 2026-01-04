e03a35f27ccae2abacc2415f3b8eaafd58fbd15e
luka1212tz <lukamodeluka12@gmail.com>
2026-01-04 20:58:59 +0400

    sixtieth commit

---
+++ level 170/class/my-project/src/App.jsx
@@ -0,0 +1,22 @@
+
+function App() {
+
+
+  return (
+    <>
+
+    <div className=" flex flex-row  justify-center align-middle  sm:flex-col">
+      
+      <img  className="sm:rounded-[50%] sm:w-[50%] sm:shadow-2xs w-full "         src="https://static.vecteezy.com/system/resources/thumbnails/057/068/323/small/single-fresh-red-strawberry-on-table-green-background-food-fruit-sweet-macro-juicy-plant-image-photo.jpg" alt="" />
+
+      <button className= " sm:text-4xl sm:rounded-[50%]  md:border-8 md:border-blue-800   border-amber-400 border-2 rounded-3xl"  > click me </button>
+
+        <p className=" sm:text-black sm:text-6xl  md:text-gray-700 md:font-bold text-red-900 ">hello this site is responsive</p></div>
+    
+
+
+    </>
+  )
+}
+
+export default App
---
+++ level 170/home/my-project/src/App.jsx
@@ -0,0 +1,31 @@
+
+function App() {
+
+
+
+
+//sm:   640px  (ტელეფონი )
+//md:   768px  (ტაბლეტი )
+//lg:   1024px (ლეპტოპი)
+//xl:   1280px (დიდი ეკრანი)
+
+  return (
+    <>
+     
+
+
+
+<p className=" text-gray-800 text-center sm:text-lg md:text-xl lg:text-2xl xl:text-3xl   ">hi my name is luka</p>
+
+
+
+<button  className="text-white rounded-lg text-sm bg-blue-600 sm:bg-green-600  md:bg-yellow-500  lg:bg-red-600 xl:bg-purple-60" ></button>
+
+
+
+
+    </>
+  )
+}
+
+export default App
